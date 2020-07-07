84582d2216dec6839a6e0e82a6cd5db6e1e20a9f
shnepal121 <56094894+shnepal121@users.noreply.github.com>
2020-07-07 14:18:09 -0400

    API Call

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -73,8 +73,9 @@ namespace APIproject.Controllers
                         // https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=CpQCAgEAAFxg8o-eU7_uKn7Yqjana-HQIx1hr5BrT4zBaEko29ANsXtp9mrqN0yrKWhf-y2PUpHRLQb1GT-mtxNcXou8TwkXhi1Jbk-ReY7oulyuvKSQrw1lgJElggGlo0d6indiH1U-tDwquw4tU_UXoQ_sj8OBo8XBUuWjuuFShqmLMP-0W59Vr6CaXdLrF8M3wFR4dUUhSf5UC4QCLaOMVP92lyh0OdtF_m_9Dt7lz-Wniod9zDrHeDsz_by570K3jL1VuDKTl_U1cJ0mzz_zDHGfOUf7VU1kVIs1WnM9SGvnm8YZURLTtMLMWx8-doGUE56Af_VfKjGDYW361OOIj9GmkyCFtaoCmTMIr5kgyeUSnB-IEhDlzujVrV6O9Mt7N4DagR6RGhT3g1viYLS4kO5YindU6dm3GIof1Q&key=YOUR_API_KEY
 
                         object has_next_page = parks.GetType().GetProperty("next_page_token"); 
-                        if (has_next_page == null) /// Check how to handle Null, should we use === while comparing string null (actual NULL)?? . 
+                        if (!has_next_page.Equals("") || has_next_page is null) /// Check how to handle Null, should we use === while comparing string null (actual NULL)?? . 
                         {
+                            Console.WriteLine("the value is:" + has_next_page); 
                             break; 
                         }
                         else
@@ -93,7 +94,7 @@ namespace APIproject.Controllers
             {
                 Console.WriteLine(e.Message);
             }
-            return View(parks);
+            return View(FinalResult);
             
         }
 
--- Models/park_result.cs
+++ Models/park_result.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// Model = [Root, Root, Root]
+//Root =>  {html_attributions, list[],status,..}
 namespace APIproject.Models
 {
     public class Root
